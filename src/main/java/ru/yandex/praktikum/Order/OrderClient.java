3067b3ac66d4db78e6c2899a3e3798440f0183d6
Aleksei Bugrov <da1tgm71@gmail.com>
2022-03-21 23:25:01 +0300

    Исправлено замечание 4. Добавлена аннотация @Step

--- src/main/java/ru/yandex/praktikum/Order/OrderClient.java
+++ src/main/java/ru/yandex/praktikum/Order/OrderClient.java
@@ -1,8 +1,8 @@
 package ru.yandex.praktikum.Order;
 
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import ru.yandex.praktikum.BaseSpec;
-import ru.yandex.praktikum.Order.Order;
 
 import static io.restassured.RestAssured.given;
 
@@ -10,14 +10,17 @@ public class OrderClient extends BaseSpec {
 
     private final String ORDER_PATH = "http://qa-scooter.praktikum-services.ru/api/v1/orders/";
 
+    @Step("Создание заказа")
     public int createOrder(Order order) {
         return given().spec(getBaseSpec()).body(order).when().post(ORDER_PATH).then().extract().statusCode();
     }
 
+    @Step("Получение списка заказов")
     public Response getListOrder() {
         return given().spec(getBaseSpec()).get(ORDER_PATH);
     }
 
+    @Step("Получение трек номера")
     public int getOrderTrack(Order order) {
         return given().spec(getBaseSpec()).body(order).when().post(ORDER_PATH).then().extract().path("track");
 
